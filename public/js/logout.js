9cb4e303be6e002d6e8b0abd1bc9e4f5fe5067e4
Alexis Mastrodomenico <86946841+LexMastro@users.noreply.github.com>
2022-01-25 18:29:40 +1100

    update

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,16 +1,16 @@
 const logout = async (e) => {
-    e.preventDefault();
-    console.log('This part');
-    const response = await fetch('/api/users/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-    });
-    console.log('res', response);
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert(response.statusText);
-    }
+  e.preventDefault();
+  console.log("Logout pressed");
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+  console.log("res", response);
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector("#logout").addEventListener("click", logout);
